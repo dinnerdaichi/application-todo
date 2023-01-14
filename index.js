964548ac1a8c7093559879521b6af2fd372c8ff7
dinnerdaichi <35797046+dinnerdaichi@users.noreply.github.com>
2023-01-14 19:04:16 +0900

    ファイルアップロード
    最初のアップロードだよ

---
+++ index.js
@@ -0,0 +1,75 @@
+const form = document.getElementById('form');
+const input = document.getElementById("input");
+const ul = document.getElementById('ul');
+const cotion = document.getElementById('cotion');
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+
+if (todos) {
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  add()
+});
+
+function add(todo) {
+  let todoText = input.value;
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const li = document.createElement('li');
+    li.innerText = todoText;
+    li.classList.add("list-item");
+
+    if ( todo && todo.complated){
+      li.classList.add("text-line")
+    }
+
+    li.addEventListener("contextmenu", function (event) {
+
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+
+    li.addEventListener("click", function () {
+      li.classList.toggle("text-line")
+      saveData();
+    })
+
+    ul.appendChild(li);
+    input.value = "";
+    saveData()
+
+
+cotion.classList.remove("display");
+    
+  }else{
+    cotion.classList.add("display");
+  }
+}
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+
+  lists.forEach(list => {
+    let todo = {
+      text: list.innerText,
+      complated: list.classList.contains("text-line")
+    };
+
+    todos.push(todo);
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+}
